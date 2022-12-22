4f8a58485c578473fb9669d7d84fe00fae134154
TSENG-TZU-YU <46403343+TSENG-TZU-YU@users.noreply.github.com>
2022-12-22 17:40:59 +0800

    file bug

--- routers/application_edit.js
+++ routers/application_edit.js
@@ -14,8 +14,8 @@ router.patch('/submit/:num', async (req, res) => {
         let r = req.body;
 
         let [application] = await pool.execute(
-            `UPDATE application_form SET  handler=?,application_category=?,project_name=?,cycle=?,status_id=? WHERE case_number=? && user_id=? `,
-            [r.handler, r.application_category, r.project_name, r.cycle, r.status, numId, r.id]
+            `UPDATE application_form SET  handler=?,application_category=?,project_name=?,cycle=?,status_id=? ,create_time=?WHERE case_number=? && user_id=? `,
+            [r.handler, r.application_category, r.project_name, r.cycle, r.status, v.create_time, numId, r.id]
         );
         // console.log('r', r);
     } catch (err) {
@@ -31,8 +31,8 @@ router.patch('/store/:num', async (req, res) => {
         let r = req.body;
 
         let [application] = await pool.execute(
-            `UPDATE application_form SET  handler=?,application_category=?,project_name=?,cycle=? WHERE case_number=? && user_id=? && status_id=?`,
-            [r.handler, r.application_category, r.project_name, r.cycle, numId, r.id, r.status]
+            `UPDATE application_form SET  handler=?,application_category=?,project_name=?,cycle=?,create_time=? WHERE case_number=? && user_id=? && status_id=?`,
+            [r.handler, r.application_category, r.project_name, r.cycle, v.create_time, numId, r.id, r.status]
         );
     } catch (err) {
         console.log(err);
@@ -47,26 +47,33 @@ router.post('/file/:num', async (req, res) => {
     let nowDate = moment().format('YYYYMM');
     const arr = Object.values(req?.files || {});
 
+    //刪除資料庫檔案
     let [deletFile] = await pool.execute(
         `DELETE FROM upload_files_detail
     WHERE case_number_id=?`,
         [numId]
     );
-    // let filePath = __dirname + `/../${v.dbTime}/${numId}`;
-    // let files = [];
-    // if (fs.existsSync(filePath)) {
-    //     files = fs.readdirSync(filePath);
-    //     files.forEach((file, index) => {
-    //         let curPath = filePath + '/' + file;
-    //         if (fs.statSync(curPath).isDirectory()) {
-    //             delDir(curPath); //遞迴刪除目錄下的資料夾
-    //         } else {
-    //             fs.unlinkSync(curPath); //刪除檔案
-    //         }
-    //     });
-    //     fs.rmdirSync(filePath); //刪除目錄
-    // }
-    // console.log('numId', numId);
+
+    //刪除後端檔案
+    if (v.dbTime.length > 1) {
+        let filePath = __dirname + `/../${v.dbTime}/${numId}`;
+        files = fs.readdirSync(filePath);
+        console.log(' files', files[2]);
+        //TODO:迴圈 讀取原本的檔案NO 有相同的在 rename 檔案no
+        // let files = [];
+        // if (fs.existsSync(filePath)) {
+        //     files = fs.readdirSync(filePath);
+        //     files.forEach((file, index) => {
+        //         let curPath = filePath + '/' + file;
+        //         if (fs.statSync(curPath).isDirectory()) {
+        //             delDir(curPath); //遞迴刪除目錄下的資料夾
+        //         } else {
+        //             fs.unlinkSync(curPath); //刪除檔案
+        //         }
+        //     });
+        //     fs.rmdirSync(filePath); //刪除目錄
+        // }
+    }
 
     for (let i = 0; i < arr.length; i++) {
         // 轉換類型名稱
