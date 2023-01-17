ec8050b03bea7243ca2198df12d1ea84822a54b1
TSENG-TZU-YU <46403343+TSENG-TZU-YU@users.noreply.github.com>
2023-01-17 17:56:57 +0800

    test

--- controllers/application.js
+++ controllers/application.js
@@ -11,80 +11,85 @@ async function addHandleState(caseNum, handler, state, remark, estTime, createTi
 // /api/1.0/applicationData?category = 1
 async function getAllApp(req, res) {
     const { category, state, unit, minDate, maxDate, order, HUnit } = req.query;
-    // let userId = req.session.member.id;
-    // let handleName = req.session.member.name;
-    // let Manage = req.session.member.manage;
-    let User = req.session.member.user;
-    // let Handler = req.session.member.handler;
-    // let Director = req.session.member.director;
+    try {
+        // let userId = req.session.member.id;
+        // let handleName = req.session.member.name;
+        // let Manage = req.session.member.manage;
+        let User = req.session.member.user;
+        // let Handler = req.session.member.handler;
+        // let Director = req.session.member.director;
 
-    // console.log('ucc',userId);
+        // console.log('ucc',userId);
 
-    // 篩選
-    let categoryVal = category ? `AND (a.application_category = '${category}')` : '';
-    let stateVal = state ? `AND (a.status_id = ${state})` : '';
-    let unitVal = unit ? `AND (u.applicant_unit = '${unit}')` : '';
-    let unitHVal = HUnit ? `AND (a.unit = '${HUnit}')` : '';
-    let dateVal =
-        minDate || maxDate ? `AND (a.create_time BETWEEN '${minDate} 00:00:00' AND '${maxDate} 23:59:59')` : '';
+        // 篩選
+        let categoryVal = category ? `AND (a.application_category = '${category}')` : '';
+        let stateVal = state ? `AND (a.status_id = ${state})` : '';
+        let unitVal = unit ? `AND (u.applicant_unit = '${unit}')` : '';
+        let unitHVal = HUnit ? `AND (a.unit = '${HUnit}')` : '';
+        let dateVal =
+            minDate || maxDate ? `AND (a.create_time BETWEEN '${minDate} 00:00:00' AND '${maxDate} 23:59:59')` : '';
 
-    let orderType = null;
-    switch (order) {
-        case '1':
-            orderType = 'a.case_number ASC';
-            break;
-        case '2':
-            orderType = 'a.case_number DESC';
-            break;
-        case '3':
-            orderType = 'a.create_time ASC';
-            break;
-        case '4':
-            orderType = 'a.create_time DESC';
-            break;
-        default:
-            orderType = 'a.create_time DESC';
-    }
+        let orderType = null;
+        switch (order) {
+            case '1':
+                orderType = 'a.case_number ASC';
+                break;
+            case '2':
+                orderType = 'a.case_number DESC';
+                break;
+            case '3':
+                orderType = 'a.create_time ASC';
+                break;
+            case '4':
+                orderType = 'a.create_time DESC';
+                break;
+            default:
+                orderType = 'a.create_time DESC';
+        }
 
-    let result = '';
-    // user permissions=1
-    if (User === 1) {
-        [result] = await pool.execute(
-            `SELECT a.*, s.name, u.applicant_unit, COUNT(d.case_number_id) sum, SUM(d.checked) cou 
+        let result = '';
+        // user permissions=1
+        if (User === 1) {
+            [result] = await pool.execute(
+                `SELECT a.*, s.name, u.applicant_unit, COUNT(d.case_number_id) sum, SUM(d.checked) cou , c.called
       FROM application_form a 
       JOIN status s ON a.status_id = s.id
       JOIN users u ON a.user_id = u.id
       JOIN application_form_detail d ON a.case_number = d.case_number_id
+      JOIN select_states_checked c ON a.case_number = c.case_number
       WHERE (a.status_id NOT IN (1)) ${categoryVal} ${stateVal} ${unitVal} ${dateVal}  ${unitHVal}
       GROUP BY d.case_number_id,s.name, u.applicant_unit
       ORDER BY ${orderType}
        `
-        );
+            );
+        }
+
+        // all申請單位
+        [unitResult] = await pool.execute(`SELECT * FROM unit`);
+
+        // all申請狀態
+        [statusResult] = await pool.execute(`SELECT * FROM status`);
+
+        // all申請類別
+        [categoryResult] = await pool.execute(`SELECT * FROM application_category`);
+
+        // all處理人
+        [handlerResult] = await pool.execute(`SELECT * FROM handler`);
+
+        // all申請人
+        [userResult] = await pool.execute(`SELECT * FROM users WHERE user = ?`, [1]);
+
+        res.json({
+            result,
+            unitResult,
+            statusResult,
+            categoryResult,
+            handlerResult,
+            userResult,
+        });
+    } catch (err) {
+        console.log(err);
     }
-
-    // all申請單位
-    [unitResult] = await pool.execute(`SELECT * FROM unit`);
-
-    // all申請狀態
-    [statusResult] = await pool.execute(`SELECT * FROM status`);
-
-    // all申請類別
-    [categoryResult] = await pool.execute(`SELECT * FROM application_category`);
-
-    // all處理人
-    [handlerResult] = await pool.execute(`SELECT * FROM handler`);
-
-    // all申請人
-    [userResult] = await pool.execute(`SELECT * FROM users WHERE user = ?`, [1]);
-
-    res.json({
-        result,
-        unitResult,
-        statusResult,
-        categoryResult,
-        handlerResult,
-        userResult,
-    });
 }
 
 // 總管理filter all data
--- controllers/application_handler.js
+++ controllers/application_handler.js
@@ -5,47 +5,48 @@ async function getAllAppHandler(req, res) {
     // console.count('被打了');
     // console.table( categoryVal); 物件
     const { category, state, unit, minDate, maxDate, order, HUnit } = req.query;
+    // console.log('ucc', req.session.member );
+    try {
+        // let userId = req.session.member.id;
+        let handleName = req.session.member.name;
+        let Manage = req.session.member.manage;
+        let Handler = req.session.member.handler;
+        let HandlerUnit = req.session.member.applicant_unit;
 
-    // let userId = req.session.member.id;
-    let handleName = req.session.member.name;
-    let Manage = req.session.member.manage;
-    let Handler = req.session.member.handler;
-    let HandlerUnit = req.session.member.applicant_unit;
+      
 
-    // console.log('ucc', Manage);
+        // 篩選
+        let categoryVal = category ? `AND (a.application_category = '${category}')` : '';
+        let stateVal = state ? `AND (a.status_id = ${state})` : '';
+        let unitHVal = HUnit ? `AND (a.unit = '${HUnit}')` : '';
+        let unitVal = unit ? `AND (u.applicant_unit = '${unit}')` : '';
+        let dateVal =
+            minDate || maxDate ? `AND (a.create_time BETWEEN '${minDate} 00:00:00' AND '${maxDate} 23:59:59')` : '';
 
-    // 篩選
-    let categoryVal = category ? `AND (a.application_category = '${category}')` : '';
-    let stateVal = state ? `AND (a.status_id = ${state})` : '';
-    let unitHVal = HUnit ? `AND (a.unit = '${HUnit}')` : '';
-    let unitVal = unit ? `AND (u.applicant_unit = '${unit}')` : '';
-    let dateVal =
-        minDate || maxDate ? `AND (a.create_time BETWEEN '${minDate} 00:00:00' AND '${maxDate} 23:59:59')` : '';
+        let orderType = null;
+        switch (order) {
+            case '1':
+                orderType = 'a.case_number ASC';
+                break;
+            case '2':
+                orderType = 'a.case_number DESC';
+                break;
+            case '3':
+                orderType = 'a.create_time ASC';
+                break;
+            case '4':
+                orderType = 'a.create_time DESC';
+                break;
+            default:
+                orderType = 'a.create_time DESC';
+        }
 
-    let orderType = null;
-    switch (order) {
-        case '1':
-            orderType = 'a.case_number ASC';
-            break;
-        case '2':
-            orderType = 'a.case_number DESC';
-            break;
-        case '3':
-            orderType = 'a.create_time ASC';
-            break;
-        case '4':
-            orderType = 'a.create_time DESC';
-            break;
-        default:
-            orderType = 'a.create_time DESC';
-    }
+        let result = '';
 
-    let result = '';
-
-    // handler permissions=3
-    if (Handler === 1) {
-        [result] = await pool.execute(
-            `SELECT a.*, s.name, u.applicant_unit, COUNT(d.case_number_id) sum, SUM(d.checked) cou
+        // handler permissions=3
+        if (Handler === 1) {
+            [result] = await pool.execute(
+                `SELECT a.*, s.name, u.applicant_unit, COUNT(d.case_number_id) sum, SUM(d.checked) cou
         FROM application_form a
         JOIN status s ON a.status_id = s.id
         JOIN users u ON a.user_id = u.id
@@ -54,14 +55,14 @@ async function getAllAppHandler(req, res) {
         GROUP BY d.case_number_id, s.name, u.applicant_unit, a.id
         ORDER BY ${orderType}
          `,
-            [handleName, handleName, HandlerUnit]
-        );
-    }
+                [handleName, handleName, HandlerUnit]
+            );
+        }
 
-    // handler permissions=4
-    if (Manage === 1) {
-        [result] = await pool.execute(
-            `SELECT a.*, s.name, u.applicant_unit, COUNT(d.case_number_id) sum, SUM(d.checked) cou
+        // handler permissions=4
+        if (Manage === 1) {
+            [result] = await pool.execute(
+                `SELECT a.*, s.name, u.applicant_unit, COUNT(d.case_number_id) sum, SUM(d.checked) cou
         FROM application_form a
         JOIN status s ON a.status_id = s.id
         JOIN users u ON a.user_id = u.id
@@ -70,36 +71,39 @@ async function getAllAppHandler(req, res) {
         GROUP BY d.case_number_id, s.name, u.applicant_unit, a.id
         ORDER BY ${orderType}
          `,
-            [0]
-        );
-        //TODO: a.status_id=4   a.status_id NOT IN (1) 效能問題
+                [0]
+            );
+            //TODO: a.status_id=4   a.status_id NOT IN (1) 效能問題
+        }
+
+        // all申請單位
+        [unitResult] = await pool.execute(`SELECT * FROM unit`);
+
+        // all申請狀態
+        [statusResult] = await pool.execute(`SELECT * FROM status`);
+
+        // all申請類別
+        [categoryResult] = await pool.execute(`SELECT * FROM application_category`);
+
+        // all處理人
+        [handlerResult] = await pool.execute(`SELECT * FROM handler`);
+
+        // all申請人
+        [userResult] = await pool.execute(`SELECT * FROM users WHERE permissions_id = ?`, [1]);
+
+        // console.log('res', unitResult);
+
+        res.json({
+            result,
+            unitResult,
+            statusResult,
+            categoryResult,
+            handlerResult,
+            userResult,
+        });
+    } catch (err) {
+        console.log(err);
     }
-
-    // all申請單位
-    [unitResult] = await pool.execute(`SELECT * FROM unit`);
-
-    // all申請狀態
-    [statusResult] = await pool.execute(`SELECT * FROM status`);
-
-    // all申請類別
-    [categoryResult] = await pool.execute(`SELECT * FROM application_category`);
-
-    // all處理人
-    [handlerResult] = await pool.execute(`SELECT * FROM handler`);
-
-    // all申請人
-    [userResult] = await pool.execute(`SELECT * FROM users WHERE permissions_id = ?`, [1]);
-
-    // console.log('res', unitResult);
-
-    res.json({
-        result,
-        unitResult,
-        statusResult,
-        categoryResult,
-        handlerResult,
-        userResult,
-    });
 }
 
 module.exports = {
