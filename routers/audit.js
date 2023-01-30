dc83b12e7e6e5d21c6c17386b4d9723252888985
Sandy <a0919613087@gmail.com>
2023-01-30 11:44:12 +0800

    add audit filter

--- routers/audit.js
+++ routers/audit.js
@@ -3,14 +3,15 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../utils/db');
 
-
 // 案件來源
 // http://localhost:3001/api/audit
 router.get('/', async (req, res) => {
-    let [result] = await pool.execute(`SELECT * FROM audit_record`);
+    const { search } = req.query;
+    console.log('object', req.query);
+    let searchVal = search ? `AND (user LIKE '%${search}%' OR record LIKE '%${search}%')` : '';
+    let [result] = await pool.execute(`SELECT * FROM audit_record ${searchVal}`);
     res.json(result);
 });
-
 
 // 匯出
 module.exports = router;
