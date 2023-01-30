49d50b9ad91d1e2f4c9660cc5cd5ceb8d5ab4ad3
Sandy <a0919613087@gmail.com>
2023-01-30 13:48:30 +0800

    update audit filter

--- routers/audit.js
+++ routers/audit.js
@@ -6,12 +6,12 @@ const pool = require('../utils/db');
 // 稽核
 // http://localhost:3001/api/audit
 router.get('/', async (req, res) => {
-    const { minDate, maxDate } = req.query;
+    const { minDate, maxDate, search } = req.query;
     try {
         // 篩選
         let dateVal = minDate || maxDate ? `(time BETWEEN '${minDate} 00:00:00' AND '${maxDate} 23:59:59')` : '';
-
-        let [result] = await pool.execute(`SELECT * FROM audit_record WHERE ${dateVal} ORDER BY time ASC`);
+        let searchVal = search ? `AND (user LIKE '%${search}%' OR record LIKE '%${search}%')` : '';
+        let [result] = await pool.execute(`SELECT * FROM audit_record WHERE ${dateVal} ${searchVal} ORDER BY time ASC`);
         res.json(result);
     } catch (err) {
         console.log(err);
