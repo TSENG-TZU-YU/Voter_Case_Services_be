d4c9f1d96f2dd7cf63dedb3a53352ece6c8943f7
TSENG-TZU-YU <46403343+TSENG-TZU-YU@users.noreply.github.com>
2023-01-31 11:55:09 +0800

    worklog

--- routers/workLog.js
+++ routers/workLog.js
@@ -7,11 +7,14 @@ const moment = require('moment');
 // 日誌
 // http://localhost:3001/api/workLog
 router.post('/', async (req, res) => {
-    let session = req.session.member;
+    let rb = req.body;
+    const { minDate, maxDate, search } = req.query;
+    let dateVal = minDate || maxDate ? `AND (time BETWEEN '${minDate} 00:00:00' AND '${maxDate} 23:59:59')` : '';
     try {
-        let [result] = await pool.execute(`SELECT * FROM worklog WHERE staff_code=? ORDER BY time DESC`, [
-            session.staff_code,
+        let [result] = await pool.execute(`SELECT * FROM worklog WHERE staff_code=? ${dateVal} ORDER BY time DESC`, [
+            rb.staff_code,
         ]);
+
         res.json(result);
     } catch (err) {
         console.log(err);
@@ -19,7 +22,7 @@ router.post('/', async (req, res) => {
 });
 
 // 新增日誌
-router.post('/post', async (req, res) => {
+router.post('/submit', async (req, res) => {
     let rb = req.body;
     let session = req.session.member;
     let nowDate = moment().format('YYYY-MM-DD HH:mm:ss');
@@ -28,6 +31,16 @@ router.post('/post', async (req, res) => {
             `INSERT INTO worklog (user,staff_code,unit,job_category,Job_description,create_time,time) VALUES (?,?,?,?,?,?,?)`,
             [session.name, session.staff_code, session.applicant_unit, rb.workCategory, rb.workLog, nowDate, rb.time]
         );
+    } catch (err) {
+        console.log(err);
+    }
+});
+// 查看日誌
+router.post('/detail', async (req, res) => {
+    let rb = req.body;
+    try {
+        let [result] = await pool.execute(`SELECT * FROM  worklog  WHERE create_time=?`, [rb.create_time]);
+        res.json(result);
     } catch (err) {
         console.log(err);
     }
