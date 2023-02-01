2d81eb922af00eddd2eeaf1a7d62b94f4dd4517d
TSENG-TZU-YU <46403343+TSENG-TZU-YU@users.noreply.github.com>
2023-02-01 09:37:48 +0800

    chart

--- routers/simpleBarChart.js
+++ routers/simpleBarChart.js
@@ -8,11 +8,11 @@ const moment = require('moment');
 router.get('/', async (req, res) => {
     const { minDate, maxDate } = req.query;
     let dateVal = minDate || maxDate ? `AND (time BETWEEN '${minDate} 00:00:00' AND '${maxDate} 23:59:59')` : '';
-    let [result] = await pool.execute(`SELECT  a.*,COUNT(f.application_category) category
-        FROM application_category a 
-        LEFT JOIN application_form f ON a.name = f.application_category    
-        GROUP BY a.name
-        ORDER BY a.id ASC`);
+    let [result] = await pool.execute(`SELECT  a.*,COUNT(f.application_category) category 
+    FROM application_category a 
+    LEFT JOIN application_form f ON a.name = f.application_category AND f.status_id NOT IN (1) 
+    GROUP BY a.name
+    ORDER BY a.id ASC`);
     res.json(result);
 });
 
